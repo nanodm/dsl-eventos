399038acb0fb50290c3328202fc68232681b9960
nanodm <nanodurandm@gmail.com>
2020-10-22 20:00:38 -0300

    logro agregar generico, cambio de lugar parsers para que sea mas intuitivo

--- Parser.hs
+++ Parser.hs
@@ -12,6 +12,17 @@ import Data.Time.Clock
 import qualified Text.ParserCombinators.Parsec.Token as Token
 import AST
 
+lis :: TokenParser u
+lis = makeTokenParser (emptyDef   { commentStart  = "/*"
+                                  , commentEnd    = "*/"
+                                  , commentLine   = "//"
+                                  , reservedNames = ["agregar", "-r", "ver", "skip", "crear", "abrir"]
+                                  , reservedOpNames = ["/","-",":",";"]
+                                  })
+
+parseComm :: SourceName -> String -> Either ParseError FileComm
+parseComm = parse (totParser fileP)
+
 -- Funciones para facilitar el testing del parser.
 totParser :: Parser a -> Parser a
 totParser p = do 
@@ -20,38 +31,9 @@ totParser p = do
                 eof
                 return t
 
-
-lis :: TokenParser u
-lis = makeTokenParser (emptyDef   { commentStart  = "/*"
-                                  , commentEnd    = "*/"
-                                  , commentLine   = "//"
-                                  , reservedNames = ["agregar", "agregarEntre", "ver", "skip", "crear", "abrir"]
-                                  , reservedOpNames = ["/","-",":",";"]
-                                  })
-
-parseComm :: SourceName -> String -> Either ParseError FileComm
-parseComm = parse (totParser fileP)
-
 fileP :: Parser FileComm
 fileP =   newFileP
       <|> openFileP
-
-comm = parens lis comm
-     <|> sequenceOfComm
-
-sequenceOfComm =
-  do list <- (sepBy1 comm' (semi lis))
-     return $ (listToSeq list)
-
-comm' :: Parser Comm
-comm' =    insertP
-       <|> insertBetweenP
-       <|> selectP
-       <|> skipComm
-
-listToSeq [] = Skip
-listToSeq [x] = x
-listToSeq (x:xs) = Seq x (listToSeq xs)
 
 newFileP :: Parser FileComm
 newFileP = do reserved lis "crear"
@@ -67,6 +49,32 @@ openFileP = do reserved lis "abrir"
                seq <- comm
                return (Open (filename ++ ".csv") seq)
 
+str :: Parser String
+str = do sp <- many space
+         x <- alphaNum
+         sp2 <- many space
+         xs <- many str
+         let string = (sp)++[x]++(sp2)++(concat xs)
+             string2 = reverse $ removeSpaces $ reverse string
+         return string2
+
+comm = parens lis comm
+     <|> sequenceOfComm
+
+sequenceOfComm =
+  do list <- (sepBy1 comm' (semi lis))
+     return $ (listToSeq list)
+
+comm' :: Parser Comm
+comm' = try insertP
+       <|>  insertBetweenP
+       <|>  selectP
+       <|>  skipComm
+
+listToSeq [] = Skip
+listToSeq [x] = x
+listToSeq (x:xs) = Seq x (listToSeq xs)
+
 skipComm :: Parser Comm
 skipComm = reserved lis "skip" >> return Skip
 
@@ -77,8 +85,9 @@ insertP = do  reserved lis "agregar"
               return (Insert date desc)
 
 insertBetweenP :: Parser Comm
-insertBetweenP = do reserved lis "agregarEntre"
+insertBetweenP = do reserved lis "agregar"
                     day1 <- dayP
+                    reserved lis "-r"
                     day2 <- dayP
                     hour <- hourP
                     desc <- str
@@ -115,11 +124,3 @@ removeSpaces :: String -> String
 removeSpaces [] = []
 removeSpaces (x:xs) = if isSpace x then removeSpaces xs else (x:xs)
 
-str :: Parser String
-str = do sp <- many space
-         x <- alphaNum
-         sp2 <- many space
-         xs <- many str
-         let string = (sp)++[x]++(sp2)++(concat xs)
-             string2 = reverse $ removeSpaces $ reverse string
-         return string2
